69dfd4aab3fd87111e60b6c6a6c42283799f7316
OneRandomGithubUser <70461479+OneRandom1509@users.noreply.github.com>
2023-11-02 23:41:14 +0530

    Added navbar

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,56 @@ import 'package:flutter/material.dart';
 import 'package:valve_control/screens/valves_screen.dart';
 
 void main() {
-  runApp(const ValveControl());
+  runApp(MaterialApp(home:ValveControl()));
 }
 
-class ValveControl extends StatelessWidget {
-  const ValveControl({Key? key}) : super(key: key);
-
+class ValveControl extends StatefulWidget {
+  @override
+  // ignore: library_private_types_in_public_api
+  _ValveControlState createState() => _ValveControlState();
+}
+class _ValveControlState extends State<ValveControl>{
+  int _currentIndex = 0;
+  _getDrawerItemWidget(int pos){
+    switch (pos){
+      case 0:
+        return ValvesScreen();
+      case 1:
+        return Placeholder();
+    }
+  }
+  //List<String> titleList = ["Home", "Scheduler"];
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: ValvesScreen());
+    final colorScheme = Theme.of(context).colorScheme;
+    final textTheme = Theme.of(context).textTheme;
+
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _currentIndex,
+        backgroundColor: colorScheme.surface,
+        selectedItemColor: colorScheme.onSurface,
+        unselectedItemColor: colorScheme.onSurface.withOpacity(.60),
+        selectedLabelStyle: textTheme.bodySmall,
+        unselectedLabelStyle: textTheme.bodySmall,
+        onTap: (value) {
+          setState(() {
+            _currentIndex = value;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            label: 'Home',
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            label: 'Scheduler',
+            icon: Icon(Icons.punch_clock),
+          ),
+        ],
+      ),
+      body: _getDrawerItemWidget(_currentIndex),
+    );
   }
 }
